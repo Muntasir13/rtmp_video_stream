c4306c941618caeb357ca8d2ff952d0b333f2e3f
Muntasir Mohammad <mohammadmuntasir96@gmail.com>
2021-04-01 16:12:55 +0600

    RTMP Video Streaming Threaded

--- app.py
+++ app.py
@@ -15,5 +15,3 @@ def index():
 @app.route("/rtmp_stream", methods=['GET'])
 def rtmp_stream():
     return Response(stream.return_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-app.run(host='0.0.0.0', port=5000, debug=True)
--- getStream.py
+++ getStream.py
@@ -1,12 +1,16 @@
+import os
 import cv2
+from camera_config.camera import CameraStream
 
 class Stream:
     def __init__(self, source):
-        self.video_source = cv2.VideoCapture(source)
+        # self.video_source = cv2.VideoCapture(source)
+        self.video_source = CameraStream(source).start()
+
 
     def return_frame(self):
         ret, frame = self.video_source.read()
         while(ret):
-            ret, frame = self.video_source.read()
             re, jpeg = cv2.imencode('.jpg', frame)
-            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tostring() + b'\r\n\r\n')
+            yield b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tostring() + b'\r\n\r\n'
+            ret, frame = self.video_source.read()
